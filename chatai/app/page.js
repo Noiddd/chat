224765e2963b2ca82768523396d03f050f3fa34e
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-04-05 21:34:19 +0800

    fresh start

---
+++ chatai/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="text-5xl font-bold mb-20">messenger</h1>
+    </div>
+  );
+}
